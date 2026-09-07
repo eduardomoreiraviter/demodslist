ddd589bb5b885e3a6ba5f4ab7bcfcdc1d22fa30b
Eduardo Viter <moreiraviter@outlook.com>
2026-09-07 06:53:29 -0300

    first commit

---
+++ demoDsList/src/main/java/com/viter/demoDsList/DemoDsListApplication.java
@@ -0,0 +1,13 @@
+package com.viter.demoDsList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDsListApplication.class, args);
+	}
+
+}
---
+++ demoDsList/src/test/java/com/viter/demoDsList/DemoDsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viter.demoDsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
