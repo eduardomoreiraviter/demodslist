3698cf196e5558a37e725cf2b8f2d67d7cad83ab
Eduardo Viter <moreiraviter@outlook.com>
2026-09-08 06:59:58 -0300

    domain model

---
+++ demoDsList/src/main/java/com/viter/demoDsList/entities/Belonging.java
@@ -0,0 +1,57 @@
+package com.viter.demoDsList.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private Integer position;
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList gameList, Integer position) {
+        id.setGame(game);
+        id.setGameList(gameList);
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id) && Objects.equals(position, belonging.position);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(position);
+        return result;
+    }
+}
