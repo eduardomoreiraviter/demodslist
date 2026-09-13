91733a054f06e9c090bd97dc1bb190410eee3135
Eduardo Viter <moreiraviter@outlook.com>
2026-09-13 16:48:41 -0300

    deploy ready

--- demoDsList/src/main/java/com/viter/demoDsList/services/GameService.java
+++ demoDsList/src/main/java/com/viter/demoDsList/services/GameService.java
@@ -8,7 +8,6 @@ import com.viter.demoDsList.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
